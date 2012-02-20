5b9a0b4856c0a8104972da6357e9ceb992a075c4
afrand08@student.aau.dk <afrand08@student.aau.dk@677157ad-1db3-4159-b65b-8943731281d4>
2012-02-20 09:49:58 +0000

    git-svn-id: svn+ssh://brian.mikkelsen.com/svn/splurge@2 677157ad-1db3-4159-b65b-8943731281d4

---
+++ code/First Prototype/src/sw8/proto1/FirstProtoPlayerActivity.java
@@ -0,0 +1,13 @@
+package sw8.proto1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstProtoPlayerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
