b2361439056b76d2ff63f68a1aeb62c2f01e708e
afrand08@student.aau.dk <afrand08@student.aau.dk@677157ad-1db3-4159-b65b-8943731281d4>
2012-02-21 08:49:40 +0000

    the playservice is set up to broadcast.
    git-svn-id: svn+ssh://brian.mikkelsen.com/svn/splurge@4 677157ad-1db3-4159-b65b-8943731281d4

--- code/First Prototype/src/sw8/proto1/PlayService.java
+++ code/First Prototype/src/sw8/proto1/PlayService.java
@@ -6,14 +6,40 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
 public class PlayService extends Service {
+
+	private static final String tag = "SW8tag.PlayService";
+	public static final String UPDATE_INFO = "com.sw802f12.playservice.update_info";
+	private final Handler handler = new Handler();
+	Intent intent;
 
 	// Binder given to clients
 	private final IBinder mBinder = new LocalBinder();
 	// Mediaplayer
 	MediaPlayer songPlayer = new MediaPlayer();
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		intent = new Intent(UPDATE_INFO);
+	}
+	
+	@Override
+    public void onStart(Intent intent, int startId) {
+        handler.removeCallbacks(sendUpdatesToUI);
+        handler.postDelayed(sendUpdatesToUI, 100); // 0.1 second   
+    }
+
+	private Runnable sendUpdatesToUI = new Runnable() {
+		public void run() {
+			updateInfo();
+			handler.postDelayed(this, 100); // 0.1 seconds
+		}
+	};
 
 	/**
 	 * Class used for the client Binder. Because we know this service always
@@ -44,9 +70,20 @@ public class PlayService extends Service {
 		}
 		songPlayer.start();
 	}
-	
-	public void die(){
+
+	public void die() {
 		songPlayer.release();
 	}
 
+	private void updateInfo() {
+		Log.d(tag, "Started building track info.");
+		//Current progress
+		int progress = (songPlayer.getCurrentPosition()/songPlayer.getDuration())*100;
+		Log.d(tag, "Current progress was: " + progress);
+		intent.putExtra("progress", progress);
+		//Any other things we might need.
+		
+		//Sending the broadcast
+		sendBroadcast(intent);
+	}
 }
