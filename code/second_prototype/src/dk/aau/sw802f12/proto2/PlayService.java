606819077b75313e18108e074639c92d046879dc
mlisby08@student.aau.dk <mlisby08@student.aau.dk@677157ad-1db3-4159-b65b-8943731281d4>
2012-03-14 08:42:00 +0000

    Added additional required files, plus updated SDK-version to 10.
    git-svn-id: svn+ssh://brian.mikkelsen.com/svn/splurge@34 677157ad-1db3-4159-b65b-8943731281d4

---
+++ code/second_prototype/src/dk/aau/sw802f12/proto2/PlayService.java
@@ -0,0 +1,5 @@
+package dk.aau.sw802f12.proto2;
+
+public class PlayService {
+
+}
---
+++ code/second_prototype/src/dk/aau/sw802f12/proto2/Song.java
@@ -0,0 +1,64 @@
+package dk.aau.sw802f12.proto2;
+
+import android.media.MediaMetadataRetriever;
+
+public class Song {
+
+	private String absPath;
+	private String name;
+	private String artist;
+	private String album;
+	private String tracknumber;
+
+	public String getAbsPath() {
+		return absPath;
+	}
+
+	public void setAbsPath(String absPath) {
+		this.absPath = absPath;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getTracknumber() {
+		return tracknumber;
+	}
+
+	public void setTracknumber(String tracknumber) {
+		this.tracknumber = tracknumber;
+	}
+
+	public Song(String abspath) {
+		this.absPath = abspath;
+		MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+		mmr.setDataSource(abspath);
+		this.name = mmr.extractMetadata(mmr.METADATA_KEY_TITLE);
+		this.artist = mmr.extractMetadata(mmr.METADATA_KEY_ARTIST);
+		this.album = mmr.extractMetadata(mmr.METADATA_KEY_ALBUM);
+		this.tracknumber = mmr.extractMetadata(mmr.METADATA_KEY_CD_TRACK_NUMBER);
+	}
+
+	
+}
