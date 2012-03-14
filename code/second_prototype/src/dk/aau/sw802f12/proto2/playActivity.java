4ddd8c89f5ca552900d807fb093433c864ab4cb0
mlisby08@student.aau.dk <mlisby08@student.aau.dk@677157ad-1db3-4159-b65b-8943731281d4>
2012-03-14 08:35:14 +0000

    Created second prototype.
    git-svn-id: svn+ssh://brian.mikkelsen.com/svn/splurge@33 677157ad-1db3-4159-b65b-8943731281d4

---
+++ code/second_prototype/src/dk/aau/sw802f12/proto2/playActivity.java
@@ -0,0 +1,13 @@
+package dk.aau.sw802f12.proto2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class playActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
